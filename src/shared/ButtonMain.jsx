9c0781af99df946b501e8174e2ead65d0f175603
itstahirrafique <hello.tahirrafique@gmail.com>
2025-02-10 16:39:51 +0500

    changes

--- src/shared/ButtonMain.jsx
+++ src/shared/ButtonMain.jsx
@@ -4,7 +4,7 @@ const ButtonMain = ({ text, expIcon, toggleDropdown, time }) => {
             <button
                 onClick={toggleDropdown}
 
-                className="navbtn-size  navbtn-border background-grad text-inverted px-4 py-1 3xl:px-6 3xl:py-2 cursor-pointer font-medium flex items-center">
+                className="navbtn-size  navbtn-border background-grad text-inverted px-4 py-2 cursor-pointer font-medium flex items-center">
                 {text}
                 <img src={expIcon} alt="" />
                 {time}
--- src/shared/ButtonSecondary.jsx
+++ src/shared/ButtonSecondary.jsx
@@ -2,7 +2,7 @@
 const ButtonSecondary = ({downArrow,text}) => {
     return (
         <div>
-            <button className="navbtn-size border border-grd text-main navbtn-border  px-4 py-1 3xl:px-6 3xl:py-2 cursor-pointer font-medium flex items-center gap-1">
+            <button className="navbtn-size border border-grd text-main navbtn-border px-4 py-2 cursor-pointer font-medium flex items-center gap-1">
                 {text}
                 <img src={downArrow} alt="" />
             </button>
