4d4754b9ff4d4ad4cfb1bb083eb0ce24686eeddb
itstahirrafique <hello.tahirrafique@gmail.com>
2025-02-13 16:55:41 +0500

    404 page responsiveness fixed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,21 +1,20 @@
 import { MdError } from "react-icons/md";
-
-
 const NotFound = () => {
     return (
-        <div className="bg-frame w-full h-screen text-center  ">
-            <div className="flex items-center justify-center">
-                <div className="text-main text-[300px]">404</div>
-                <MdError className="text-[300px] text-main" />
-            </div>
-            <h1 className="text-main pb-10">page not found.....!</h1>
+        <>
+            <section className="bg-frame  h-[100vh] text-center py-10 ">
+                <div className="flex items-center justify-center">
+                    <div className="text-main text-5xl sm:text-[270px]">
+                        404
+                    </div>
+                    <MdError className="text-5xl sm:text-[200px] text-main" />
+                </div>
+                <h1 className="text-main pb-10">page not found.....!</h1>
 
-            <a href="/" className="text-main text-2xl border-2 px-5 py-3 rounded-2xl hover:bg-main hover:text-inverted ">Go Back...! </a>
-
-
-
-        </div>
+                <a href="/" className="text-main text-2xl border-2 px-5 py-3 rounded-2xl hover:bg-main hover:text-inverted ">Go Back...! </a>
+            </section>
+        </>
     )
 }
 
-export default NotFound
+export default NotFound;
