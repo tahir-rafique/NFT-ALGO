6d6ea47726653d6059cd30e84bf8561066f0239f
itstahirrafique <hello.tahirrafique@gmail.com>
2025-02-18 12:31:31 +0500

    footer website-links responsiveness updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,20 +32,17 @@ const Footer = () => {
             <h2 className="text-center font-bold uppercase">Ready for Next NFT Drop?</h2>
 
             <div className="mx-auto rounded-xl flex justify-between items-center gap-4 px-10  py-6 footer-border drop-shadow-xl relative z-50 w-full max-w-[650px]">
-              <input className="font-secondary  text-main w-full max-w-[480px] outline-none " placeholder="info@gmail.com" />
+              <input className="font-secondary  text-main w-full max-w-[480px] outline-none"
+                placeholder="info@gmail.com" />
               <a href="#" className="absolute navbtn-size text-small navbtn-border font-medium background-grad text-inverted px-4 py-2 shrink-0 cursor-pointer flex gap-1 max-md:right-4 right-8">
                 <img src={message} alt="no img" />
               </a>
             </div>
-
           </div>
 
-          {/* footer end */}
+          {/* footer-End */}
           <div className="footer-bg px-10 lg:px-20 relative  py-5 lg:py-18 overflow-y-hidden">
-
             <img src={tracked} alt="" className="absolute top-0 left-0 max-lg:hidden  z-1 size-99" />
-
-            {/* main footer text taqreebn-360 */}
             <div className="py-2 flex flex-col max-lg:items-center lg:flex-row relative z-50">
               <div className="w-[40%] max-lg:w-full flex justify-center ">
                 <a href="/">
@@ -55,11 +52,12 @@ const Footer = () => {
               </div>
 
               <div className="w-[60%] flex gap-10 max-lg:flex-col max-lg:w-full items-center">
-
                 <div className="Social-links w-[70%] max-lg:w-full flex flex-col gap-10">
                   <div>
                     <h5 className="max-lg:text-center font-secondary text-inverted">NFT ALGO is the world’s leading NFTs marketplace where you can discover, sell and bid NFTs and get rich </h5>
                   </div>
+
+                  {/* Social-Links */}
                   <div className="flex gap-2 max-lg:justify-center">
                     <img src={fb} alt="fb" className="cursor-pointer" />
                     <img src={telegram} alt="telegram" className="cursor-pointer" />
@@ -72,27 +70,25 @@ const Footer = () => {
                   </div>
                 </div>
 
-
+                {/* website-Links */}
                 <div className="font-secondary text-inverted  w-[30%] max-lg:w-full max-lg:flex-row max-lg:justify-center max-lg:items-center max-lg:gap-4 flex flex-col lg:gap-5 ">
                   <div className="flex flex-col gap-2">
-
-                    <div className="max-lg:flex-row flex max-lg:gap-4 lg:flex-col lg:gap-4">
+                    <div className="flex max-lg:justify-center">
                       <h3 className="cursor-pointer font-bold font-secondary">About</h3>
+                    </div>
+                    <div className="max-lg:flex-row flex flex-col max-lg:gap-4 max-lg:justify-center lg:gap-4">
                       <h5 className="cursor-pointer">About NFT</h5>
                       <h5 className="cursor-pointer">Live Auctions</h5>
-                    </div>
-
-                    <div className="max-lg:flex-row flex flex-col max-lg:gap-4 max-lg:justify-center lg:gap-4">
                       <h5 className="cursor-pointer">Collection</h5>
                       <h5 className="cursor-pointer">Activity</h5>
                     </div>
-
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
+
+
         </div>
       </div>
     </div>
